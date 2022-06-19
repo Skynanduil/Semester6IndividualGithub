f0ec39b802ffa447b158ea32936bd12ffa977e63
Stefan <Stefanzulfikarkaya@hotmail.com>
2022-06-19 22:54:33 +0200

    Add FAAS to frontend

---
+++ walking skeleton/services/frontend/src/modules/FAAS.js
@@ -0,0 +1,30 @@
+import React from "react";
+import getFunctionCalledCountFromFAAS from '../api/FAAS_API_CALLS'
+
+class FAAS extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {count: 0};
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        getFunctionCalledCountFromFAAS().then(
+            res => {
+                alert(res.data.message);
+                this.state.count = res.data.count;
+            }
+        );
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label>Count: {this.state.count}</label>
+                <button type= "submit">Call FAAS function</button>
+            </form>
+        )
+    }
+}
