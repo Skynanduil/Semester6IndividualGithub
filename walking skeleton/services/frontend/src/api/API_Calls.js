431f065285be64f7f61894fd629586cf5a28ed52
Stefan <Stefanzulfikarkaya@hotmail.com>
2022-06-20 02:54:29 +0200

    Remove Security for now

--- walking skeleton/services/frontend/src/api/API_Calls.js
+++ walking skeleton/services/frontend/src/api/API_Calls.js
@@ -1,41 +1,25 @@
 import API from './API'
-import getAccessToken from '../auth/getAccesToken';
-
-const getAuthConfig = async () =>{
-    let accessToken = await getAccessToken();
-    let config = {
-        headers: {
-            'Authorization': 'Bearer ' + accessToken
-        }
-    }
-
-    return config;
-}
 
 export function getStaticResponseFromAPI(){
     return API.get('/producer/static')
 }
 
-export async function sendMessageToMessageBroker(message){
-    let config = await getAuthConfig();
-    return API.post('/producer/produce?message=' + message, config);
+export function sendMessageToMessageBroker(message){
+    return API.post('/producer/produce?message=' + message)
 }
 
-export async function createMessageInDB(title, content){
-    let config = await getAuthConfig();
-    return API.post('/producer/message/create', {title: title, content: content}, config);
+export function createMessageInDB(title, content){
+    return API.post('/producer/message/create', {title: title, content: content})
 }
 
 export function getMessageFromDB(id){
     return API.get('/producer/message/' + id)
 }
 
-export async function editMessageInDB(id, title, content){
-    let config = await getAuthConfig();
-    return API.put('/producer/message/edit', {id: id, title: title, content: content}, config);
+export function editMessageInDB(id, title, content){
+    return API.put('/producer/message/edit', {id: id, title: title, content: content})
 }
 
-export async function deleteMessageInDB(id){
-    let config = await getAuthConfig();
-    return API.delete('/producer/message/' + id, config);
+export function deleteMessageInDB(id){
+    return API.delete('/producer/message/' + id)
 }
--- walking skeleton/services/gateway/src/main/java/walking/skeleton/gateway/security/SecurityConfig.java
+++ walking skeleton/services/gateway/src/main/java/walking/skeleton/gateway/security/SecurityConfig.java
@@ -1,35 +1,35 @@
-package walking.skeleton.gateway.security;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.*;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@EnableWebFluxSecurity
-public class SecurityConfig{
-    @Value("${auth0.audience")
-    private String audience;
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuer;
-
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
-        http
-                .authorizeExchange()
-                .pathMatchers(HttpMethod.GET,"/gateway/producer/**").permitAll()
-                .anyExchange().authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-        return http.build();
-    }
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder(){
-        return ReactiveJwtDecoders.fromOidcIssuerLocation(issuer);
-    }
-}
+//package walking.skeleton.gateway.security;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.oauth2.jwt.*;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@EnableWebFluxSecurity
+//public class SecurityConfig{
+//    @Value("${auth0.audience")
+//    private String audience;
+//
+//    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+//    private String issuer;
+//
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+//        http
+//                .authorizeExchange()
+//                .pathMatchers(HttpMethod.GET,"/gateway/producer/**").permitAll()
+//                .anyExchange().authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder(){
+//        return ReactiveJwtDecoders.fromOidcIssuerLocation(issuer);
+//    }
+//}
